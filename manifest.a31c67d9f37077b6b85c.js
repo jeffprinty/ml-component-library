614edd9a90c95fa8ddab3be9f6f4f368b3b46e07
Jeff Printy <jeff.printy.contractor@macmillan.com>
2017-02-03 15:04:07 -0600

    Updates

---
+++ manifest.a31c67d9f37077b6b85c.js
@@ -0,0 +1,122 @@
+/******/!function(e){/******/
+/******/
+// The require function
+/******/
+function t(n){/******/
+/******/
+// Check if module is in cache
+/******/
+if(a[n])/******/
+return a[n].exports;/******/
+/******/
+// Create a new module (and put it into the cache)
+/******/
+var r=a[n]={/******/
+exports:{},/******/
+id:n,/******/
+loaded:!1};/******/
+/******/
+// Return the exports of the module
+/******/
+/******/
+/******/
+// Execute the module function
+/******/
+/******/
+/******/
+// Flag the module as loaded
+/******/
+return e[n].call(r.exports,r,r.exports,t),r.loaded=!0,r.exports}// webpackBootstrap
+/******/
+// install a JSONP callback for chunk loading
+/******/
+var n=window.webpackJsonp;/******/
+window.webpackJsonp=function(c,o){/******/
+for(/******/
+// add "moreModules" to the modules object,
+/******/
+// then flag all "chunkIds" as loaded and fire callback
+/******/
+var i,p,f=0,s=[];f<c.length;f++)/******/
+p=c[f],/******/
+r[p]&&/******/
+s.push.apply(s,r[p]),/******/
+r[p]=0;/******/
+for(i in o){/******/
+var l=o[i];/******/
+/******/
+// Check if module is deduplicated
+/******/
+switch(typeof l){/******/
+case"object":/******/
+// Module can be created from a template
+/******/
+e[i]=function(t){/******/
+var n=t.slice(1),a=t[0];/******/
+return function(t,r,c){/******/
+e[a].apply(this,[t,r,c].concat(n))}}(l);/******/
+break;/******/
+case"function":/******/
+// Normal module
+/******/
+e[i]=l;/******/
+break;/******/
+default:/******/
+// Module is a copy of another module
+/******/
+e[i]=e[l]}}/******/
+for(/******/
+n&&n(c,o);s.length;)/******/
+s.shift().call(null,t);/******/
+if(o[0])/******/
+/******/
+return a[0]=0,t(0)};/******/
+/******/
+// The module cache
+/******/
+var a={},r={/******/
+0:0};/******/
+/******/
+// This file contains only the entry chunk.
+/******/
+// The chunk loading function for additional chunks
+/******/
+t.e=function(e,n){/******/
+// "0" is the signal for "already loaded"
+/******/
+if(0===r[e])/******/
+return n.call(null,t);/******/
+/******/
+// an array means "currently loading".
+/******/
+if(void 0!==r[e])/******/
+r[e].push(n);else{/******/
+// start chunk loading
+/******/
+r[e]=[n];/******/
+var a=document.getElementsByTagName("head")[0],c=document.createElement("script");/******/
+c.type="text/javascript",/******/
+c.charset="utf-8",/******/
+c.async=!0,/******/
+/******/
+c.src=t.p+""+({1:"app",2:"vendor"}[e]||e)+"."+{1:"506149bee0a1fcb3d579",2:"7699284fab738fb6a45c"}[e]+".js",/******/
+a.appendChild(c)}},/******/
+/******/
+// expose the modules object (__webpack_modules__)
+/******/
+t.m=e,/******/
+/******/
+// expose the module cache
+/******/
+t.c=a,/******/
+/******/
+// __webpack_public_path__
+/******/
+t.p="./"}(function(e){
+// Check all modules for deduplicated modules
+for(var t in e)if(Object.prototype.hasOwnProperty.call(e,t))switch(typeof e[t]){case"function":break;case"object":
+// Module can be created from a template
+e[t]=function(t){var n=t.slice(1),a=e[t[0]];return function(e,t,r){a.apply(this,[e,t,r].concat(n))}}(e[t]);break;default:
+// Module is a copy of another module
+e[t]=e[e[t]]}return e}([]));
+//# sourceMappingURL=manifest.a31c67d9f37077b6b85c.js.map
