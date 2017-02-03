880936c45b58819346be01c9476ae06322ca3d24
Jeff Printy <jeff.printy.contractor@macmillan.com>
2017-02-03 10:34:48 -0600

    Updates

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
-'use strict';
+import 'babel-polyfill'; // IE polyfill for promises
+import { AppContainer } from 'react-hot-loader';
+import React from 'react';
+import { render } from 'react-dom';
 
-require('babel-polyfill');
+import _Root from './components/_Root/_Root';
 
-var _reactHotLoader = require('react-hot-loader');
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _reactDom = require('react-dom');
-
-var _Root2 = require('./components/_Root/_Root');
-
-var _Root3 = _interopRequireDefault(_Root2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// IE polyfill for promises
-(0, _reactDom.render)(_react2.default.createElement(
-  _reactHotLoader.AppContainer,
-  null,
-  _react2.default.createElement(_Root3.default, null)
-), document.getElementById('root-entry'));
+render(
+  <AppContainer>
+    <_Root />
+  </AppContainer>,
+  document.getElementById('root-entry')
+);
 
 // Handle hot reloading requests from Webpack
 if (module.hot) {
-  module.hot.accept('./components/_Root/_Root', function () {
+  module.hot.accept('./components/_Root/_Root', () => {
     //If we receive a HMR request for our App container, then
     //reload it using require (we can't do this dynamically with import)
-    var NextApp = require('./components/_Root/_Root').default;
+    const NextApp = require('./components/_Root/_Root').default;
 
     // And render it into the root element again
-    (0, _reactDom.render)(_react2.default.createElement(
-      _reactHotLoader.AppContainer,
-      null,
-      _react2.default.createElement(NextApp, null)
-    ), document.getElementById('root-entry'));
+    render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root-entry')
+    );
   });
 }
-;
-
-var _temp = function () {
-  if (typeof __REACT_HOT_LOADER__ === 'undefined') {
-    return;
-  }
-}();
-
-;
